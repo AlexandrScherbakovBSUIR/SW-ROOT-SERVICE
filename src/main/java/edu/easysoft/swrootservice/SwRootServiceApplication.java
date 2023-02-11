cd886731903243da0f97eecc9d3ac87edd96d8ac
unknown <alexandrscherbakov231194@gmail.com>
2023-02-11 12:33:13 +0200

    initial commit

---
+++ src/main/java/edu/easysoft/swrootservice/SwRootServiceApplication.java
@@ -0,0 +1,13 @@
+package edu.easysoft.swrootservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwRootServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SwRootServiceApplication.class, args);
+    }
+
+}
